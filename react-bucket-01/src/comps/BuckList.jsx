5a995dd2279c92850a631dfd7a28ebde17881201
soyeon-noh <1010qwerty12@gmail.com>
2021-09-13 16:43:13 +0900

    react_bucatList

--- react-bucket-01/src/comps/BuckList.jsx
+++ react-bucket-01/src/comps/BuckList.jsx
@@ -3,13 +3,24 @@ import BuckItem from "./BuckItem";
 
 function BuckList(props) {
   /**
-   * 부모 컴포넌트로부터 전달받은 bucketList를 사용하기 위해
-   * 변수로 선언하기
+   * 부모 컴포넌트로 부터 전달받은 bucketList를
+   * 사용하기 위해 변수로 선언하기
+   *
+   * BucketMain에서 보낸 배열변수와 2개의 함수를
+   * props로 부터 분리해 내기
    */
-  const { bucketList, flag_change } = props;
+  const { bucketList, flag_change, bucket_update, bucket_complete } =
+    props.args;
 
   const buckBody = bucketList.map((bucket) => {
-    return <BuckItem bucket={bucket} flag_change={flag_change} />;
+    return (
+      <BuckItem
+        args={props.args}
+        bucket={bucket}
+        // flag_change={flag_change}
+        // bucket_update={bucket_update}
+      />
+    );
   });
 
   return (
@@ -17,10 +28,10 @@ function BuckList(props) {
       <thead>
         <tr>
           <th>FLAG</th>
-          <th>DATE</th>
+          <th>날짜</th>
           <th>BUCKET</th>
-          <th>FINISH</th>
-          <th>CANCEL</th>
+          <th>완료</th>
+          <th>취소</th>
         </tr>
       </thead>
       <tbody>{buckBody}</tbody>
--- react-bucket-01/src/comps/BuckMain.jsx
+++ react-bucket-01/src/comps/BuckMain.jsx
@@ -1,33 +1,33 @@
 import React, { useState } from "react";
 import BuckList from "./BuckList";
 import BuckInput from "./BuckInput";
+import uuid from "react-uuid";
+import moment from "moment";
 
 function BuckMain() {
-  const [bucketList, setBuckList] = useState([
-    //리스트를만들기떄문에 []
-    {
-      b_id: 0,
-      b_flag: 0,
-      b_start_date: "2021-09-13",
-      b_title: "리액트 정복",
-      b_end_date: "",
-      b_end_check: false,
-      b_cancel: false,
-    },
-    {
-      b_id: 1,
-      b_flag: 0,
-      b_start_date: "2021-09-13",
-      b_title: "추석맞이",
-      b_end_date: "",
-      b_end_check: false,
-      b_cancel: false,
-    },
-  ]);
+  // 버킷리스트를 담을 배열
+  const [bucketList, setBuckList] = useState([]);
 
+  const buck_insert = (bucket_text) => {
+    const bucket = {
+      b_id: uuid(),
+      b_start_date: moment().format("YYYY[-]MM[-]DD HH:mm:ss"),
+      b_title: bucket_text,
+      b_flag: 0,
+      b_end_date: "",
+      b_end_check: false,
+      b_cancel: false,
+    };
+    // 원래있던 bucketList에 새로운 bucket을 추가하기
+    setBuckList([...bucketList, bucket]);
+  };
+
+  // 리스트에서 FLAG항목을 클릭하면 실행할 함수
   const flag_change = (id) => {
-    bucketList.map((bucket) => {
-      //   alert(bucket.b_flag);
+    const _bucketList = bucketList.map((bucket) => {
+      /**
+       * 전달받은 id와 같은 항목의 flag를 1 증가시키기
+       */
       if (bucket.b_id === id) {
         return {
           ...bucket,
@@ -37,13 +37,64 @@ function BuckMain() {
         return bucket;
       }
     });
-    setBuckList([...bucketList]);
+    // 원래의 bucketList를 _bucketList로 바꾸기
+    setBuckList(_bucketList);
   };
+
+  // 리스트에서 input box에 버킷을 변경한 후 Enter를 누르면
+  // 실행할 함수
+  const bucket_update = (id, title) => {
+    const _bucketList = bucketList.map((bucket) => {
+      if (bucket.b_id === id) {
+        // b_id가 id값과 같으면
+        // bucket에 담긴 항목중에서
+        // b_title 항목만 변경하여 통째로 return
+        return { ...bucket, b_title: title };
+      } else {
+        // b_id가 id와 같지 않으면
+        // 아무것도 변경없이 bucket을 그대로 return
+        return bucket;
+      }
+    });
+    // 원래의 list를 새로운 list로 바꾸기
+    setBuckList(_bucketList);
+  };
+
+  const bucket_complet = (id) => {
+    bucketList.map((bucket) => {
+      if (bucket.b_id === id) {
+        return {
+          ...bucket,
+          b_end_date: moment().format("YYYY[-]MM[-]DD HH:mm:ss"),
+          b_end_check: true,
+        };
+      } else {
+        return bucket;
+      }
+    });
+  };
+
+  const args = {
+    bucketList: bucketList,
+    flag_change: flag_change,
+    bucket_update: bucket_update,
+    bucket_complet: bucket_complet,
+  };
+
   return (
     <div className="w3-container-fluid">
-      <BuckInput />
-      {/** BuckList 컴포넌트에 buckketList 상태(변수) 전달하기 */}
-      <BuckList bucketList={bucketList} flag_change={flag_change} />
+      <BuckInput buck_insert={buck_insert} />
+      {/* BuckList 컴포넌트에 bucketList 상태(변수) 전달하기  */}
+      {/* 
+	  BucketItem.jsx에서 실행할 flag_change() bucket_update() 
+	  함수를 매개변수로 전달하기
+	  */}
+      <BuckList
+        args={args}
+        // bucketList={bucketList}
+        // flag_change={flag_change}
+        // bucket_update={bucket_update}
+      />
     </div>
   );
 }
